7c245379a60e5d5874016e0df5833798a4d2f61a
Dan Clarke <dan@dracan.co.uk>
2014-10-25 20:02:04 +0100

    Moved the TagSetListItem class from the TagSelection.xaml.cs file into the TimeTrackR.Core project. Not sure why I put it there initially!

---
+++ TimeTrackR.Core/Tags/TagSetListItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimeTrackR.Core.Tags
+{
+    public class TagSetListItem : IEquatable<TagSetListItem>
+    {
+        public IEnumerable<string> Tags { get; set; }
+
+        public string TagsAsString
+        {
+            get { return string.Join(", ", Tags); }
+        }
+
+        public bool Equals(TagSetListItem other)
+        {
+            // Check whether the compared object is null
+            if(ReferenceEquals(other, null)) return false;
+
+            // Check whether the compared object references the same data
+            if(ReferenceEquals(this, other)) return true;
+
+            // Check whether the objects' properties are equal
+            return TagsAsString.Equals(other.TagsAsString);
+        }
+
+        public override int GetHashCode()
+        {
+            return TagsAsString.GetHashCode();
+        }
+    }
+}
--- TimeTrackR/TagSelection.xaml.cs
+++ TimeTrackR/TagSelection.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using TimeTrackR.Core.Tags;
@@ -7,33 +6,6 @@ using TimeTrackR.Core.Timer;
 
 namespace TimeTrackR
 {
-    public class TagSetListItem : IEquatable<TagSetListItem>
-    {
-        public IEnumerable<string> Tags { get; set; }
-
-        public string TagsAsString
-        {
-            get { return string.Join(", ", Tags); }
-        }
-
-        public bool Equals(TagSetListItem other)
-        {
-            // Check whether the compared object is null
-            if(ReferenceEquals(other, null)) return false;
-
-            // Check whether the compared object references the same data
-            if(ReferenceEquals(this, other)) return true;
-
-            // Check whether the objects' properties are equal
-            return TagsAsString.Equals(other.TagsAsString);
-        }
-
-        public override int GetHashCode()
-        {
-            return TagsAsString.GetHashCode();
-        }
-    }
-
     /// <summary>
     /// Interaction logic for TagSelection.xaml
     /// </summary>
