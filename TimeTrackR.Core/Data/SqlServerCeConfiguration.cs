42d69f90c9d3eac5e9b65944b7e3e727a6c051b6
Dan Clarke <dan@dracan.co.uk>
2014-11-01 20:47:50 +0000

    Added SqlServerCeConfiguration class to specify the provider so that it's not required in an app.config.

---
+++ TimeTrackR.Core/Data/SqlServerCeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServerCompact;
+
+namespace TimeTrackR.Core.Data
+{
+    public class SqlServerCeConfiguration : DbConfiguration
+    {
+        public SqlServerCeConfiguration()
+        {
+            SetProviderServices(SqlCeProviderServices.ProviderInvariantName, SqlCeProviderServices.Instance);
+        }
+    }
+}
